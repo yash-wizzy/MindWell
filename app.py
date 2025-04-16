9b841ab322bc1cd1e8e177e846c800a875939295
Yash Khandelwal <yash.dbl2004@gmail.com>
2025-04-17 04:55:15 +0530

    update

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, render_template, jsonify
 import os
-from utils.extract_audio import extract_mfcc_from_video
-from utils.extract_text import get_glove_embedding
+from utils.extract_audio import extract_audio_from_video, extract_mfcc  
+from utils.extract_text import get_text_embedding  
 from utils.extract_visual import run_openface
 import torch
-import numpy as np
 from model import DepressionModel, weights_init
 
 app = Flask(__name__)
@@ -21,25 +20,29 @@ model.eval()
 def index():
     if request.method == 'POST':
         video = request.files['video']
-        text = request.form['text']
-
+        
         video_path = os.path.join(app.config['UPLOAD_FOLDER'], video.filename)
         video.save(video_path)
-
-        audio_feat = extract_mfcc_from_video(video_path)
-        text_feat = get_glove_embedding(text)
+        
+        audio_path = os.path.join(app.config['UPLOAD_FOLDER'], "temp_audio.wav")
+        extract_audio_from_video(video_path, audio_path)        
+        
+        audio_feat = extract_mfcc(audio_path)
+        transcribed_text = get_text_embedding(audio_path) 
         visual_feat = run_openface(video_path)
-       
-        audio_tensor = torch.tensor(audio_feat, dtype=torch.float32).unsqueeze(0)   
-        visual_tensor = torch.tensor(visual_feat, dtype=torch.float32).unsqueeze(0)   
-        text_tensor = torch.tensor(text_feat, dtype=torch.float32).unsqueeze(0)
-
+      
+        audio_tensor = torch.tensor(audio_feat, dtype=torch.float32).unsqueeze(0)
+        text_tensor = torch.tensor(transcribed_text, dtype=torch.float32).unsqueeze(0)
+        visual_tensor = torch.tensor(visual_feat, dtype=torch.float32).unsqueeze(0)
 
         with torch.no_grad():
-           prediction = model(audio_tensor, text_tensor, visual_tensor).item()
-        
+            prediction = model(audio_tensor, text_tensor, visual_tensor).item()
 
-        return jsonify({'depression_score': prediction})
+        os.remove(audio_path)
+        
+        return jsonify({
+            'depression_score': prediction
+        })
 
     return render_template('index.html')
 
